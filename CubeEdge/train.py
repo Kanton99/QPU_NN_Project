b50ecf8281fa13fed4d2b4f08268a022350373c4
MuseBall <ureachkeerthana@gmail.com>
2023-07-16 20:44:18 +0200

    added loss history to test and train functions

--- CubeEdge/train.py
+++ CubeEdge/train.py
@@ -47,15 +47,18 @@ def test(data, model):
     num_batches = len(dataloader)
     model.eval()
     test_loss, correct = 0, 0
+    loss_history = []
     with torch.no_grad():
         for X, y in dataloader:
             X, y = X.to(device), y.to(device)
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
+            loss_history.append(loss_fn(pred, y).item())
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
     test_loss /= num_batches
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    return loss_history
 
 if __name__=="__main__":
 
